9fb0600added0893b22b1b41e0577849da685fd5
niya1123 <yoheikurioka@gmail.com>
2018-09-24 17:05:09 +0900

    ?????

--- js/test.js
+++ js/test.js
@@ -7,9 +7,11 @@ var ASSETS = {
     // 地面
     'ground': 'https://rawgit.com/alkn203/tomapiko_void/master/assets/image/ground.png',
     'human': './img/human.png',
+    'monster': './img/Monster.png',
   },
   spritesheet:{
     'human_ss': 'https://api.myjson.com/bins/1bu3s4',
+    'slime': 'https://api.myjson.com/bins/gkuis',
   }, 
    
 };
@@ -17,6 +19,10 @@ var ASSETS = {
 var SCREEN_WIDTH   = 640; // 画面横サイズ
 var SCREEN_HEIGHT  = 640; // 画面縦サイズ
 var PLAYER_SIZE    = 64;  // プレイヤーのサイズ
+var PLAYER_SPEED   = 6;   // プレイヤーの速度
+var ENEMY_SIZE     = 64;
+var ENEMY_MAX_NUM  = 6;
+var ENEMY_INTERVAL = 300;
 /*
  * メインシーン
  */
@@ -45,6 +51,27 @@ phina.define("MainScene", {
      // プレイヤー作成
      this.player = Player().addChildTo(this)
      .setPosition(grid.span(0.5), grid.span(8.75));
+     //敵グループ
+     this.enemyGroup = DisplayElement().addChildTo(this);
+     //最初の敵生成
+     this.generateEnemy();
+  },
+  //更新処理
+  update: function(app){
+    var enemys = this.enemyGroup.children;
+    if(app.frame % ENEMY_INTERVAL === 0 && enemy.length < ENEMY_MAX_NUM){
+        this.generateEnemy();
+    }  
+  },
+  //タッチの処理
+  onpointstart: function(){
+    this.player.reflectX();
+  },
+  //敵生成処理
+  generateEnemy: function(){
+    var x = this.gridX.span(Random.randint(2,14));
+    var y = this.gridY.span(Random.randint(2,3));
+    Enemy().addChildTo(this.enemyGroup).setPosition(x,y);
   },
 });
 /*
@@ -59,8 +86,42 @@ phina.define("Player", {
       this.superInit('human', PLAYER_SIZE, PLAYER_SIZE);
       // スプライトにフレームアニメーションをアタッチ
       FrameAnimation('human_ss').attachTo(this).gotoAndPlay('right_walk');
+      //移動
+      this.physical.velocity.x = PLAYER_SPEED;
     },
-  });
+    //更新処理
+    update: function(){
+        //画面左
+        if(this.left < 0){
+            this.left = 0;
+            this.reflectX();
+        }
+        //画面右
+        if(this.right > SCREEN_WIDTH){
+            this.right = SCREEN_WIDTH;
+            this.reflectX();
+        }
+    },
+    //反転処理
+    reflectX: function(){
+        //移動方向反転
+        this.physical.velocity.x *= -1;
+        //向き反転
+        this.scaleX *= -1;
+    },
+});
+
+/**
+ * 敵クラス
+ */
+phina.define("Enemy",{
+    superClass: 'Sprite',
+
+    init: function(){
+        this.superInit('monster', ENEMY_SIZE, ENEMY_SIZE);
+        FrameAnimation('slime').attachTo(this).gotoAndPlay('slime-left');
+    },
+});
 /*
  * 地面クラス
  */
